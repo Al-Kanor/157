ebdc1389e24b426fd5705e48a099156cacc581ce
victo0 <v.loffet@rubika-edu.com>
2015-03-12 19:03:37 +0100

    Scoreboard v2
    -Mise à jour du scoreboard
    -Ajout d'avatars diverses pour les joueurs affiché dans le scoreboard.

---
+++ Assets/Scripts/UI/AutoRefresh.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AutoRefresh : MonoBehaviour {
+	
+
+	void Start () {
+	}
+	
+}
