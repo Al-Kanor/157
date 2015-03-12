d007db86a807336456d14615ef21f4f8fb49dc19
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2015-03-12 14:25:24 +0100

    Début de l'intégration des assets graphiques
    - Les blocs minables sont intégrés

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -50,8 +50,7 @@ public class Player : MonoBehaviour {
         transform.position = Vector3.Lerp (transform.position, targetPos, speed * Time.deltaTime);
 
         if (Vector3.Distance (transform.position, targetPos) < 0.05f) {
-            // Clamp
-            transform.position = targetPos;
+            transform.position = targetPos; // Clamp
 
             isMovable = true;
         }
