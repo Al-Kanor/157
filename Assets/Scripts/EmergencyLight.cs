ed1d3c94ca36f11d80103f40f70d04c10735d2e5
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2015-03-13 16:37:15 +0100

    Ajout d'une lumière d'urgence
    - Ajout d'une lumière rouge clignotante qui s'active pour les X
    dernières secondes (X est tweakable)
    - Ajout du timer global (2 minutes 37)
    - Correction de la position de la torche du joueur (qui était placée à
    côté de lui)

---
+++ Assets/Scripts/EmergencyLight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmergencyLight : MonoBehaviour {
+    #region Attributs publics
+    public float blinkDuration = 1;
+    #endregion
+
+    #region Attributs privés
+    private bool isLighted = true;
+    #endregion
+
+    #region Méthodes privées
+    IEnumerator Blink() {
+        do {
+            GetComponent<Light> ().enabled = isLighted;
+            isLighted = !isLighted;
+            yield return new WaitForSeconds (blinkDuration);
+        } while (true);
+    }
+
+    void Start () {
+        StartCoroutine ("Blink");
+    }
+    #endregion
+}
