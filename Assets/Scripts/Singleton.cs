5dd592cf5a44c7002a7cd57b71d7dfffb61893e5
victo0 <v.loffet@rubika-edu.com>
2015-03-16 11:19:03 +0100

    Edit Singleton

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -30,7 +30,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         if (_instance == null)
         {
-            DontDestroyOnLoad(this);
+            //DontDestroyOnLoad(this);
         }
         else
         {
